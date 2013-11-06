5d5c93d847352ae31b8dbe74e3ccfb39941815bf
Matt <mjordan@mines.edu>
2013-11-06 15:54:59 -0700

    Made an index file and began marking up the header for our pages.

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+<header>
+	<div id="title">
+		<h1>StoryShare</h1><br>
+		<h2>A place to let your imagination run wild!</h2>
+	</div>
+	<ul id="menu">
+		<li class="selected">
+			<a href="./">Home</a>
+		</li>
+		<li>
+			<a href="./">All Prompts</a>
+		</li>
+		<li>
+			<a href="./">All Stories</a>
+		</li>
+		<li class="signin">
+			<a href="./">Sign In</a>
+		</li>
+	</ul>
+</header>
+</body>
+</html>
