ac225b8983e7c6a80fe1d2ee0218a14f10715e5a
Matt <mjordan@mines.edu>
2013-11-07 00:36:46 -0700

    More work.

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="theme.css">
+	<style type="text/css">
+		.featured {
+			margin-top: 80px;
+			margin-left: 80px;
+			margin-right: 80px;
+
+			border-style: solid;
+			border-width: 1px;
+
+			padding: 20px;
+			padding-top: 0px;
+
+			background-color: #8493CA;
+		}
+
+		.featured .featured_bottom {
+			margin-top: 40px;
+		}
+
+		.featured h3 {
+			display: inline;
+		}
+
+		.featured button {
+			float: right;
+		}
+	</style>
 </head>
 <body>
 <header>
@@ -18,10 +45,34 @@
 		<li>
 			<a href="./">All Stories</a>
 		</li>
+		<li>
+			<a href="./">Weekly Trophies</a>
+		</li>
+		<li class="signin">
+			<a href="./">Your Profile</a>
+		</li>
 		<li class="signin">
 			<a href="./">Sign In</a>
 		</li>
 	</ul>
 </header>
+
+<section class="featured">
+	<h1>Featured Prompt: Titletitletitle</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed commodo metus, in gravida felis. Sed porttitor, ipsum eu semper dignissim, neque lorem lacinia dui, eget sagittis mauris sem quis ante. Etiam bibendum justo at risus aliquam feugiat. Sed imperdiet lorem at malesuada mollis. Sed sed neque in velit aliquet rutrum. Suspendisse hendrerit, leo vitae suscipit aliquet, nunc justo ornare mauris, nec ultricies diam neque dictum sem. Pellentesque feugiat tempor orci et porta. Sed vestibulum arcu vitae mi faucibus pretium. Mauris sed lacus eget dui consequat rhoncus ac et lorem. Aenean eu odio velit. Quisque gravida dictum semper. Duis porttitor orci dui, in rutrum nisi egestas venenatis.</p>
+	<div class="featured_bottom">
+		<h3>Submitted by Matthew Jordan</h3>
+		<a href="./"><button type="button">Finish The Story...</button></a>
+	</div>
+</section>
+
+<section class="featured">
+	<h1>Featured Story: Titletitletitle</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed commodo metus, in gravida felis. Sed porttitor, ipsum eu semper dignissim, neque lorem lacinia dui, eget sagittis mauris sem quis ante. Etiam bibendum justo at risus aliquam feugiat. Sed imperdiet lorem at malesuada mollis. Sed sed neque in velit aliquet rutrum. Suspendisse hendrerit, leo vitae suscipit aliquet, nunc justo ornare mauris, nec ultricies diam neque dictum sem. Pellentesque feugiat tempor orci et porta. Sed vestibulum arcu vitae mi faucibus pretium. Mauris sed lacus eget dui consequat rhoncus ac et lorem. Aenean eu odio velit. Quisque gravida dictum semper. Duis porttitor orci dui, in rutrum nisi egestas venenatis.</p>
+	<div class="featured_bottom">
+		<h3>Submitted by Matthew Jordan</h3>
+		<a href="./"><button type="button">Read the rest...</button></a>
+	</div>
+</section>
 </body>
 </html>
