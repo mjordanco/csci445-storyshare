4244105b4d65880c714d568fd4a50656e4c234e3
Sarah Falkenstein <sfalkens@mines.edu>
2013-11-12 22:07:28 -0700

    Initial CSS for story page

--- stories.php
+++ stories.php
@@ -3,10 +3,38 @@
 	<?php
 		include_once('phplibs.php');
 	?>
+	
+<style>
+	#center {
+		margin-top: 80px;
+		margin-left: 150px;
+		height: 700px;
+		background: white;
+		border: 1px solid black;
+		border-radius: 5px;
+	}	
+	
+	.storyBox {
+		margin-top: 25px;
+		margin-left: 50px;
+		margin-right: 50px;
+		border: 1px dashed black;
+		border-radius: 10px;
+		padding: 15px 15px 15px 15px;
+	}
+	
+</style>
 </head>
 <body>
 	<?php
 		print_header("stories");
 	?>
+	
+	<div id="center">
+	
+
+	</div>
+	</div>
+	
 </body>
 </html>
