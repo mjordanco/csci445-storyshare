3576e85a53977eaed710b61fc59acd8b83070c9e
Sarah Falkenstein <sfalkens@mines.edu>
2013-12-01 13:35:24 -0700

    Updated some CSS, preping to work on All Prompts and All Stories

--- prompts.php
+++ prompts.php
@@ -24,16 +24,18 @@
 	<div class="sidebar" id="second"><a href="addLink">ADVENTURE</a></div>
 	<div class="sidebar" id="third"><a href="addLink">COMEDY</a></div>
 	<div class="sidebar" id="fourth"><a href="addLink">DRAMA</a></div>
-	<div class="sidebar" id="fifth"><a href="addLink">HISTORY</a></div>
-	<div class="sidebar" id="sixth"><a href="addLink">HORROR</a></div>
-	<div class="sidebar" id="seventh"><a href="addLink">ROMANCE</a></div>
-	<div class="sidebar" id="eigth"><a href="addLink">SCI-FI</a></div>
+    <div class="sidebar" id="fifth"><a href="addLink">FANTASY</a></div>
+	<div class="sidebar" id="sixth"><a href="addLink">HISTORY</a></div>
+	<div class="sidebar" id="seventh"><a href="addLink">HORROR</a></div>
+	<div class="sidebar" id="eigth"><a href="addLink">ROMANCE</a></div>
+	<div class="sidebar" id="ninth"><a href="addLink">SCI-FI</a></div>
+    <div class="sidebar" id="tenth"><a href="addLink">OTHER</a></div>
 	
 	<div id="center">
 		<h2 id="genreHeader">Current Genre: Action</h2>
 		<table name= "pTable" border="1">
 		<?php
-			get_prompts( "action");
+			get_prompts("Action");
 		?>
 	
 	</table>
